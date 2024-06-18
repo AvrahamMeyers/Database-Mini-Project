6d53e42428b77b8069fdc1d7c45b44eea3d5e7a8
AvrahamMeyers <a32100meyers@gmail.com>
2024-06-18 22:13:42 +0300

    added indexes and ran queries with indexes and logged output

---
+++ Constraints.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7713771f363b53f16185097d8381a99490cf3c55dc57a533c40ffcae57e6c553
+size 191
--- queries.sql
+++ queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4890b7bf52d76368a05c3e37e09c08b3a1f6cd37277c80c543af55215ddde919
-size 5869
+oid sha256:4cf2144cfef9f4d8f2df81a3a45c8ae6093775251e55bf3fb8e85e2650a7ca27
+size 5810
