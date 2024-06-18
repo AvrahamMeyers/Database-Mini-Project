401876ef3cb935826ea5325197373038ed348964
AvrahamMeyers <a32100meyers@gmail.com>
2024-06-18 22:24:58 +0300

    added constraints

--- Constraints.sql
+++ Constraints.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7713771f363b53f16185097d8381a99490cf3c55dc57a533c40ffcae57e6c553
-size 191
+oid sha256:1bfeb360badb0b3cb659c1e56696bf883a2d4427c2a2e56fcc890fada4d9f320
+size 2455
